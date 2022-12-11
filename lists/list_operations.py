45edd932bd15d3ee5ced7376fee491b0666b2107
zaidkamil <xaidmetamorphos@gmail.com>
2022-12-11 12:31:08 +0530

    list

---
+++ lists/list_operations.py
@@ -0,0 +1,14 @@
+x = [1,2,3,4,5]
+# duplicating a list
+y = x * 2
+print(y)
+# concatenating a list
+a = [1,2,3]
+b = [4,5,6]
+c = [7,8,9]
+print(a+b+c)
+# deleting a list
+del(x, y) # remove x and y from memory
+# deleting a list element
+del(a[0]) # remove the first element of a
+print(a)
