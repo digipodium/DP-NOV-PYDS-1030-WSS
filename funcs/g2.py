19e4076131a1e756578f5841417373fa455ceda6
zaidkamil <xaidmetamorphos@gmail.com>
2022-12-25 12:22:20 +0530

    actor

--- funcs/g2.py
+++ funcs/g2.py
@@ -4,13 +4,23 @@ WIDTH = 800
 HEIGHT = 500
 
 square = Rect((400, 250), (50, 50))
+b = Rect((200, 250), (50, 50))
+
+b_vx = 4 # global variable
 
 def draw():
     screen.clear()
     screen.draw.filled_rect(square, 'red')
+    screen.draw.filled_rect(b, 'blue')
 
 def update():
-    if square.bottom < HEIGHT:
-        square.y += 1
-
+    global b_vx # this allows us to change the value of b_vx
+    # loop around the screen
+    square.x += 2
+    if square.x > WIDTH:
+        square.x = 0
+    # bounce the box    
+    b.x += b_vx
+    if b.right > WIDTH or b.left < 0:
+        b_vx = -b_vx # invert the direction of the box
 pgzrun.go()
