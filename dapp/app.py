a8b59c49b06009987a6a57ac215d8d98c8cf490e
Zaid Kamil <xaidmetamorphos@gmail.com>
2023-02-24 11:58:23 +0530

    streamlit #1

--- dapp/app.py
+++ dapp/app.py
@@ -13,7 +13,6 @@ import plotly.graph_objects as go
 
 # global variables
 years = list(range(1980, 2014)) # list of years from 1980 to 2013
-
 # Loading the data and preprocessing it for the sake of humanity
 @st.cache_data
 def load_dataset():
@@ -27,4 +26,26 @@ def load_dataset():
 
 with st.spinner('Loading data...'):
     df = load_dataset()
-    st.balloons()
+    # st.balloons()
+
+# UI
+countries = df.index.tolist()
+sel_country = st.selectbox('Select a country', countries)
+st.info(f'You selected {sel_country}')
+
+# KPI
+total_immigrants = df.loc[sel_country, 'Total']
+avg_immigrants = df.loc[sel_country, years].mean()
+st.subheader("Key Performance Indicators")
+c1, c2, c3 = st.columns(3)
+c1.metric("Total Immigrants", f'{total_immigrants} people', '👨‍👩‍👧‍👦')
+c2.metric('Avg. Immigrants/yr', f'{round(avg_immigrants)} people', '👨‍👩‍👧‍👦/Year')
+c3.metric("Total years", f'{len(years)} years', '📅')
+
+country_df =  df.loc[sel_country, years]
+fig = px.area(y=country_df, x=country_df.index, title=f'{sel_country} immigrants to Canada')
+fig2 = px.scatter(y=country_df, x=country_df.index, title=f'{sel_country}')
+
+c1, c2 = st.columns(2)
+c1.plotly_chart(fig, use_container_width=True)
+c2.plotly_chart(fig2, use_container_width=True)
